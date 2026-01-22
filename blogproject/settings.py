9ee6f87098a7c14db257e5f8cb96d71bfb5490e3
sanketh0411 <sankethkumar1726@gmail.com>
2026-01-22 20:31:54 +0530

    Errors Handled

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-on^!t3ukx1@m21mvzo#*tmb9hj51=hjlghb8g4#!p!h=9+fag)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
